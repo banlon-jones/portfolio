c596dc9afbf7b946c293faee72488d3da0fbc864
banlonjones <banlonjones@go-groups.net>
2021-12-13 21:22:57 +0100

    mobile menu added

---
+++ js/script.js
@@ -0,0 +1,20 @@
+let hamburgerBtn = document.querySelector('.hamburger-btn');
+let header = document.querySelector('#app-header');
+let menuItems = document.querySelectorAll('.menu-item');
+hamburgerBtn.addEventListener('click', ()=>{
+    header.classList.toggle('container');
+    document.querySelector('.close-btn').classList.toggle('close');
+    document.querySelector('.logo').style.visibility='hidden';
+    document.querySelector('.menu').classList.toggle('menu-a');
+    menuItems.forEach((item) => item.classList.toggle('menu-item-a'));
+});
+
+menuItems.forEach((item) => {
+   item.addEventListener('click', () =>{
+       header.classList.toggle('container');
+       document.querySelector('.close-btn').classList.remove('close');
+       document.querySelector('.logo').style.visibility='visible';
+       document.querySelector('.menu').classList.remove('menu-a');
+       menuItems.forEach((item) => item.classList.remove('menu-item-a'));
+   });
+});
