5885dda0eff83124e497a1ab6e22d45f8a59f422
banlonjones <banlonjones@go-groups.net>
2021-12-13 21:40:11 +0100

    fix linter errors

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-let hamburgerBtn = document.querySelector('.hamburger-btn');
-let header = document.querySelector('#app-header');
-let menuItems = document.querySelectorAll('.menu-item');
-hamburgerBtn.addEventListener('click', ()=>{
-    header.classList.toggle('container');
-    document.querySelector('.close-btn').classList.toggle('close');
-    document.querySelector('.logo').style.visibility='hidden';
-    document.querySelector('.menu').classList.toggle('menu-a');
-    menuItems.forEach((item) => item.classList.toggle('menu-item-a'));
+const hamburgerBtn = document.querySelector('.hamburger-btn');
+const header = document.querySelector('#app-header');
+const menuItems = document.querySelectorAll('.menu-item');
+hamburgerBtn.addEventListener('click', () => {
+  header.classList.toggle('container');
+  document.querySelector('.close-btn').classList.toggle('close');
+  document.querySelector('.logo').style.visibility = 'hidden';
+  document.querySelector('.menu').classList.toggle('menu-a');
+  menuItems.forEach((item) => item.classList.toggle('menu-item-a'));
 });
 
 menuItems.forEach((item) => {
-   item.addEventListener('click', () =>{
-       header.classList.toggle('container');
-       document.querySelector('.close-btn').classList.remove('close');
-       document.querySelector('.logo').style.visibility='visible';
-       document.querySelector('.menu').classList.remove('menu-a');
-       menuItems.forEach((item) => item.classList.remove('menu-item-a'));
-   });
+  item.addEventListener('click', () => {
+    header.classList.toggle('container');
+    document.querySelector('.close-btn').classList.remove('close');
+    document.querySelector('.logo').style.visibility = 'visible';
+    document.querySelector('.menu').classList.remove('menu-a');
+    menuItems.forEach((item) => item.classList.remove('menu-item-a'));
+  });
 });
