90e0fa8ba545d4247a5b3f67f36593b48c9bed64
banlonjones <banlonjones@go-groups.net>
2021-12-15 15:45:12 +0100

    linter error fixed

--- js/script.js
+++ js/script.js
@@ -14,35 +14,36 @@ const projects = [
       + 'typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since'
       + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.'
       + 'when an unknown printer took a galley of type and scrambled it 1960s with the relea',
-	role: 'Back End Dev',
-	year: '2015',
-	employer: 'CANOPY',
+    role: 'Back End Dev',
+    year: '2015',
+    employer: 'CANOPY',
   },
   {
     id: '2',
-	name: 'Multi-Post Stories',
-	description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    name: 'Multi-Post Stories',
+    description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     feature_image: 'images/Snapshoot1.png',
     technologies: ['html', 'css', 'javaScript', 'Ruby on rails'],
     details: 'Lorem Ipsum is simply dummy text of the printing and '
-      + 'typesetting industry. Lorem Ipsum has been the industry standard dummy'
-      + 'text ever since the 1500s, when an unknown printer took a galley of type and'
+      + 'typesetting industry. Lorem Ipsum has been the industry\'s standard dummy '
+      + 'text ever since the 1500s, when an unknown printer took a galley of type and '
       + 'scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and'
-      + 'typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since'
-      + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry.'
+      + 'typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since'
+      + 'the 1500s, when an unknown printer took a galley of type and scrambled it 1960s'
+      + 'with the releorem Ipsum is simply dummy text of the printing and typesetting industry.'
       + 'when an unknown printer took a galley of type and scrambled it 1960s with the relea',
     live_version: 'https://banlon-jones.github.io/portfolio/',
-    source: "https://github.com/banlon-jones/portfolio.git",
-    role: "Full Stack Dev",
-    year: "2015",
-    employer: "FACEBOOK",
+    source: 'https://github.com/banlon-jones/portfolio.git',
+    role: 'Full Stack Dev',
+    year: '2015',
+    employer: 'FACEBOOK',
   },
   {
     id: '3',
     name: 'Facebook 360',
     description: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     feature_image: 'images/Snapshoot2.png',
-    technologies: ['html', 'css', 'javaScript'],
+    technologies: ['html', 'css', 'javaScript', 'Ruby on rails'],
     details: 'Lorem Ipsum is simply dummy text of the printing and '
       + 'typesetting industry. Lorem Ipsum has been the industry standard dummy'
       + 'text ever since the 1500s, when an unknown printer took a galley of type and'
@@ -82,6 +83,7 @@ const getProject = (id) => {
     if (item.id === id) {
       return item;
     }
+    return null;
   });
   return project;
 };
@@ -105,7 +107,7 @@ menuItems.forEach((item) => {
     document.querySelector('.logo').style.visibility = 'visible';
     document.querySelector('.menu').classList.remove('menu-a');
     menuItems.forEach((item) => item.classList.remove('menu-item-a'));
-	});
+  });
 });
 
 projects.forEach((item) => {
@@ -155,7 +157,8 @@ const projecBtns = document.querySelectorAll('.project-btn');
 projecBtns.forEach((item) => {
   item.addEventListener('click', () => {
     const pro = getProject(item.getAttribute('value'));
-    modal.innerHTML = `<div>
+    modal.innerHTML = `
+    <div>
 \t\t\t\t<div class="item">
 \t\t\t\t\t<div class="card">
 \t\t\t\t\t\t<div class="card-b">
@@ -202,20 +205,22 @@ projecBtns.forEach((item) => {
 \t\t\t\t\t\t</div>
 \t\t\t\t\t</div>
 \t\t\t\t</div>
-\t\t\t</div>`;
+\t\t\t</div>
+    `;
     modal.style.visibility = 'visible';
-	modal.style.display = 'block';
+    modal.style.display = 'block';
 
-	const closeButton = document.querySelector('.clos');
-	  closeButton.addEventListener('click', () => {
-		modal.style.visibility = 'hidden';
-		modal.style.display = 'none';
-	  });
-	});
+    const closeButton = document.querySelector('.clos');
+    closeButton.addEventListener('click', () => {
+      modal.style.visibility = 'hidden';
+      modal.style.display = 'none';
+    });
+  });
 });
 
+
 const contactForm = document.getElementById('contact-form');
-const email = contactForm.elements['email'];
+const email = contactForm.elements.email;
 const regex = /[A-Z]/g;
 const errorMgs = document.querySelector('.error-mgs');
 
